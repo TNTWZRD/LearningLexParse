f95efced3256a402d26b1101dc413afbc6bb48cb
Daniel Jajliardo <tntwzrd.minecraft@gmail.com>
2017-01-22 22:42:46 -0500

    Log Manager OBJ Created and initialized

--- JRVS/main.cpp
+++ JRVS/main.cpp
@@ -1,12 +1,18 @@
 #include <iostream>
+#include <string>
 
 #include "src\utils\logManager.h"
 
 using namespace std;
 
 int main() {
+	// Variables:
+	static const string TITLE = "JRVS";
+	logger::LogManager Log = logger::LogManager::get(logger::AllLogs::AllOn);
 
+	// Do Stuff:
 
+	Log.LOGCUSTOM(TITLE, "JRVS Process Started.");
 
 
 	cout << endl;
